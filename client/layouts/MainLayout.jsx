629b83ace76553b19070fbb130e20a035f1e28a2
Brianne Hostutler <briannehostutler@gmail.com>
2016-07-05 19:59:50 -0400

    fixing logo again

--- client/layouts/MainLayout.jsx
+++ client/layouts/MainLayout.jsx
@@ -4,7 +4,7 @@ import AccountsUI from '../accounts/AccountsUI.jsx';
 export const MainLayout = ({content}) => (
   <div className="main-layout">
     <header>
-      <img src="https://bytebucket.org/briannehostutler/final-project/raw/11d918bb21054707ff516e2a721ad326cf318264/public/bone.png?token=e614c69798372d09fb02c69871b044c97451ee4c" width="150" height="100"/>
+      <img src="https://bytebucket.org/briannehostutler/final-project/raw/ea902539ac9bccd941e001ef9885f01afa6167d5/public/images/bone.png?token=725b526e2d6925964ad4a773e611e81d44cb230b" width="250" height="100"/>
 
       <nav>
         <a href="/">Home</a>
